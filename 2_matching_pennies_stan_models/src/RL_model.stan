35225eecbac5fa406d32784a71fcbe5f521e8226
ElisiusKLP <elisius@live.dk>
2026-02-27 10:03:20 +0100

    RL_vs_biased to deal with weird stan fit

--- 2_matching_pennies_stan_models/src/RL_model.stan
+++ 2_matching_pennies_stan_models/src/RL_model.stan
@@ -1,5 +1,5 @@
 data{
-# All data is observable 
+// All data is observable 
   int<lower = 1> t; // Trial numbers (t)
   array[t] int choice; // One choice for each trial
   array[t] int feedback; // One piece of feedback for each trial
@@ -7,17 +7,32 @@ data{
 }
 
 parameters{
-# All parameters are inferable
+// All parameters are inferable
   real<lower = 0, upper = 1> alpha; // We have a learning rate (alpha) that we want to infer
 }
 
 model{
-  array[t] real<lower = 0, upper = 1> V; // Each trial has a belief/rate (V)
+  array[t] real V; // Each trial has a belief/rate (V)
   V[1] = initialV; // Set the first belief to the hardcoded initialV
   
+  // Priors
+  target += normal_lpdf(alpha | 0, 1.5 );
+  
   for (i in 2:t){ // Ensures that we have first trial to get feedback and belief from
-    V[i] ~ V[i-1] + alpha * (feedback[i-1] - V[i-1]);
-    choice[i] ~ binomial(1, V[i]);
-  }
+    V[i] = V[i-1] + inv_logit(alpha) * (feedback[i-1] - V[i-1]);
+    target += binomial_lpmf(choice[i] | 1, V[i]);
+  }  
 }
+
+generated quantities {
+  real<lower = 0, upper = 1> alpha_prior;
+  
+  
+  alpha_prior = inv_logit(normal_rng(0, 1.5));
+  
+  
+} 
+
+
+
 
---
+++ 2_matching_pennies_stan_models/src/pre_fitting.R
@@ -0,0 +1,56 @@
+# set working dir
+print(getwd())
+target_dir <- "/Users/peli/Projects/Repositories/Advanced_Cognitive_Modelling_2026/2_matching_pennies_stan_models"
+if (basename(getwd()) != "2_matching_pennies_stan_models") {
+  setwd(target_dir)
+}
+print(getwd())
+print(list.files("."))
+# imports
+pacman::p_load("tidyverse", "cmdstanr", "here")
+
+fpath <- "data/RL_vs_biased.csv"
+simdata <- read_csv(fpath)
+
+# Maybe choose some specific data or loop across it.
+testdata <- simdata %>% 
+  select(agent_id, trial, choicesA, winA, learningRate, noise) %>% 
+  filter(learningRate == 0.5, noise == 0)
+
+inspect_data <- simdata %>% 
+  select(agent_id, trial, choicesA, winA, learningRate, noise, winB, ) %>% 
+  filter(learningRate == 0.5, noise == 0)
+
+initialV <- 0.5
+
+# setup stan data structure
+sdata <- list(
+  t = length(testdata$choicesA),
+  choice = testdata$choicesA,
+  feedback = testdata$winA,
+  initialV = initialV
+)
+
+rlmodelpath <- here("2_matching_pennies_stan_models", "src", "RL_model.stan")
+print(rlmodelpath)
+outputdir <- "output"
+model_file <- here(outputdir, "RL_fit.rds")
+
+rlmodel <- cmdstan_model(rlmodelpath) # create the stan model object
+
+fit_rl <- rlmodel$sample( # set configuations
+  data=sdata,
+  seed=231,
+  chains= 4,
+  parallel_chains = 4,
+  iter_warmup = 1000,
+  iter_sampling = 2000,
+  refresh = 500
+)
+
+# Save the fitted model object
+#fit_rl$save_object(file= model_file)
+fit_rl$summary()
+
+
+
