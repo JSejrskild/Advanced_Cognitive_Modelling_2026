0af1b131fd461f9da14e3d219906320c517c93e6
ElisiusKLP <elisius@live.dk>
2026-04-27 15:25:47 +0200

    diagnostics and trace plots

--- 3_bayesian_agents/src/empirical/diagnostics_empirical.R
+++ 3_bayesian_agents/src/empirical/diagnostics_empirical.R
@@ -26,17 +26,23 @@ color_scheme_set("viridis")
 output_dir <- here(workdir, "output")
 print(output_dir)
 
-# List all files in the output_dir with full paths
-agents <- c("PBA", "WBA")
+# define a helper function
+make_regex_pars <- function(bases, ids) {
+  ids_regex <- paste(ids, collapse = "|")
+  
+  unlist(lapply(bases, function(b) {
+    paste0(b, "\\[(", ids_regex, ")\\]")
+  }))
+}
 
-# Create the agent-scenario combinatory grid
-combinations <- expand.grid(
-  agent = agents
-)
+make_slices <- function(ids, n_slices) {
+  split(ids, cut(ids, breaks = n_slices, labels = FALSE))
+}
 
 # Define the function to validate model fit
-validate_model_fit <- function(agent, output_dir) {
-  modelfit_path <- here::here(output_dir, paste0(agent,"_precogsci_modelfit.rds"))
+validate_model_fit <- function(agent, output_dir, figures_dir) {
+  
+  modelfit_path <- here::here(output_dir, paste0(agent, "_precogsci_modelfit.rds"))
   print(paste0("Running model validation procedure for: ", modelfit_path))
   
   if (!file.exists(modelfit_path)) {
@@ -47,57 +53,61 @@ validate_model_fit <- function(agent, output_dir) {
   fit_object <- readRDS(modelfit_path)
   draws_df <- as_draws_df(fit_object$draws())
   
-  # Select parameters based on agent
-  if (agent == "PBA") {
-    pars <- "p"
-  } else {
-    pars <- c("rho", "kappa", "wd", "ws")
+  #diagnose_fit(fit_object)
+  
+  make_slices <- function(ids, n_slices) {
+    split(ids, cut(ids, breaks = n_slices, labels = FALSE))
   }
   
-  # Generate trace plot
-  trace_plot <- bayesplot::mcmc_trace(
-    draws_df,
-    pars = pars,
-    facet_args = list(ncol = 1, strip.position = "left")
-  ) +
-    labs(title = paste0(agent))
+  # We create 3 
+  subject_slices <- make_slices(1:45, 3)
   
-  # Return a named list
-  list(
-    plot = trace_plot,
-    agent = agent,
-    scenario = scenario
-  )
-}
-
-# Loop over combinations and validate each model fit
-results <- combinations %>%
-  pmap(~ validate_model_fit(..1, output_dir))
-
-# Split results by agent
-create_combined_trace_plot <- function(agent_tag, dir){
-  "
-  This function collects all trace plots for each agent and combines them into a wrapped plot and saves to /figures.
-  "
-  plots <- results[sapply(results, function(x) !is.null(x) && x$agent == agent_tag)] %>%
-    map("plot")
-  
-  combined_plots <- patchwork::wrap_plots(plots, ncol = 2)
-  
-  ggsave(
-    filename = here(figures_dir, paste(agent_tag, "_trace_plots.pdf")),
-    plot = combined_plots,
-    width = 8.27,
-    height = 11.7,
-    units = "in",
-    device = "pdf",
-    create.dir = TRUE
-  )
+  for (i in seq_along(subject_slices)) {
+    print(paste("Creating trace plots for subjects:", i))
+    
+    subset_ids <- subject_slices[[i]]
+    
+    # Select the parameters to plot in trace plots
+    if (agent == "PBA") {
+      pars <- c("p")
+    } else {
+      pars <- c("rho", "kappa", "wd", "ws")
+    }
+    
+    for (par in pars){
+      regex_pars <- make_regex_pars(
+        bases = par,
+        ids = subset_ids
+      )
+      print(paste("This is the regex_pars:",regex_pars)) # DEBUG Statement
+      
+      trace_plot <- bayesplot::mcmc_trace(
+        draws_df,
+        regex_pars = regex_pars
+      ) +
+      ggplot2::labs(title = paste0(agent,"| Par: ", par, " | Subjects ", min(subset_ids), "-", max(subset_ids)))
+      
+      ggsave(
+        filename = here::here(
+          figures_dir,
+          paste0(agent, "_par_", par, "_subjects_", min(subset_ids), "_to_", max(subset_ids), "_trace.png")
+        ),
+        plot = trace_plot,
+        width = 8.27,
+        height = 11.7,
+        units = "in",
+        device = "png",
+        create.dir = TRUE
+      )
+    }
+  }
 }
 
 figures_dir <- here(workdir, "figures") # figures dir
 
-for (agent in agents){
-  create_combined_trace_plot(agent,figures_dir)
-}
+# List all files in the output_dir with full paths
+agents <- c("PBA", "WBA")
 
+for (agent in agents){
+  validate_model_fit(agent, output_dir = output_dir, figures_dir = figures_dir)
+}
